35a9f75183df70166e1333ecd3b0a5ac083a8545
christiano0407 <placebo0704@gmail.com>
2024-03-04 08:56:21 -0600

    Add: System Design with Tailwind CSS and Create Landing Page

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,22 @@
 export default {
   content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        "mobile-s": "320px",
+        "mobile-m": "375px", 
+        "mobile-l":"425px", 
+      }
+    },
+    colors: {
+      "clr-White": "#f2f2f2",
+      "primary": "#44AFF2", 
+      "secondary": "#F20F79",
+      "third": "#F2913D",
+      "primary-dark": "#138AF2", 
+      "primary-light": "#04ADBF", 
+    }
   },
   plugins: [],
 }
-
+/* S-320, M-375, L-425, */
