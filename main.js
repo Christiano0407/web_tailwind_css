42a95876035a28971cd63fbb0d844dc1affdb40c
christiano0407 <placebo0704@gmail.com>
2024-03-06 09:21:48 -0600

    add new slider scroll images

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 //** ==== Travel Website ===== */
 (() => {
-   
   const slideImages = [ 
     {img: "./src/img/travel_one.jpg", title: "Trip & Dream", description:"Travel and Live your Dreams"},
     {img: "./src/img/travel_two.jpg", title: "Think Travel & Dream", description:"Travel and Live your Dreams"},
@@ -11,7 +10,7 @@
 
   const createHtmlStructure = (sliderSelector, images) => {
 
-    const parent = document.querySelector( sliderSelector, images );
+    const parent = document.querySelector( sliderSelector);
 
     // Slides
     images.forEach((slideImg, index) => {
@@ -25,7 +24,7 @@
         <div class="content">
           <div class="name">${title}</div>
           <div class="description">${description}</div>
-          <button>Ver más</button>
+          <button class="action">Explore</button>
         </div>
       </div>
       `;
@@ -34,14 +33,14 @@
     });
 
     // Buttons
-    const html = `
+    /* const html = `
     <div class="buttons">
       <button class="prev"> Prev</button>
       <button class="next"> Next</button>
     </div>
-    `;
-    const fragment = document.createRange().createContextualFragment(html);
-    parent.parentElement.appendChild(fragment);
+    `; */
+    /* const fragment = document.createRange().createContextualFragment(html);
+    parent.parentElement.appendChild(fragment); */
     
   };
 
@@ -54,18 +53,86 @@
   const $next = document.querySelector(".next");
   const $prev = document.querySelector(".prev");
   const $container = document.querySelector(".container");
+  const $action = document.querySelector(".action");
+  //const items = document.querySelectorAll(".item");
+  // Swipe Scroll (Táctil)
+  let startX = 0; 
+  let endX = 0;
+  // Mouse Events
+  let isMouseDown = false;
 
-  // Listeners
-  $next.addEventListener( 'click', () => {
+  // Initial Background Images 
+  $container.style.backgroundImage = `url('${slideImages[0].img}')`
+
+  // Listeners === Events Click and Swipe ===
+  /* $next.addEventListener( 'click', () => {
     const items = document.querySelectorAll(".item");
     $slider.appendChild(items[0]);
-  });
+  }); */
 
-  $prev.addEventListener( 'click', () => {
+  /* $prev.addEventListener( 'click', () => {
     const items = document.querySelectorAll(".item");
     $slider.prepend(items[items.length - 1],items[0]);
+  }); */
+
+  // - Swipe (Táctil) -
+  /* $slider.addEventListener("touchstart", (e) => {
+    startX = e.touches[0].clientX;
+  }); */
+
+  /* $slider.addEventListener("touchmove", (e) => {
+    endX = e.touches[0].clientX;
+  }); */
+
+  /* $slider.addEventListener("touchend", () => {
+    const diffX = startX - endX;
+    if(diffX > 50) {
+      const items = document.querySelector(".item"); 
+      const selectedItem = items[0]; 
+      $slider.appendChild(selectedItem);
+      $container.style.backgroundImage = `url('${selectedItem.style.backgroundImage.slice(5, -2)}')`;
+    }else if(diffX < -50 ) {
+      const items = document.querySelector(".item"); 
+      const selectedItem = items[0]; 
+      $slider.insertBefore(selectedItem, items[items.length - 1]);
+      $container.style.backgroundImage = `url('${selectedItem.style.backgroundImage.slice(5, -2)}')`;
+    }
+  });  */
+
+  // - Mouse -
+  $slider.addEventListener("mousedown", (e) => {
+    isMouseDown = true; 
+    startX = e.clientX; 
   });
 
+  $slider.addEventListener("mousemove", (e) => {
+    if(isMouseDown) {
+      endX = e.clientX;
+    }
+  }); 
+
+  $slider.addEventListener("mouseup", () => {
+    if(isMouseDown) {
+      const diffX = startX - endX;
+
+      if(diffX > 50) {
+        const items = document.querySelectorAll(".item"); 
+        const selectedItem = items[0]; 
+        if(selectedItem instanceof Node) {
+          $slider.appendChild(selectedItem);
+        }
+        $container.style.backgroundImage = `url('${selectedItem.style.backgroundImage.slice(5, -2)}')`;
+      }else if(diffX < -50 ) {
+        const items = document.querySelectorAll(".item"); 
+        const selectedItem = items[0]; 
+        $slider.insertBefore(selectedItem, items[items.length - 1]);
+        $container.style.backgroundImage = `url('${selectedItem.style.backgroundImage.slice(5, -2)}')`;
+      }
+    }
+    isMouseDown = false; 
+  }); 
+
+  // - Event Click Image -
   $slider.addEventListener('click', (e) => {
     const items = document.querySelectorAll(".item");
     const selectedItem = e.target.closest(".item");
